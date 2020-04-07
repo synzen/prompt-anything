d3ddba437c26917ce6d70e396bbc752963a86861
synzen <michael994664@gmail.com>
2020-04-07 14:15:17 -0400

    Fix some docs

--- src/PhaseRunner.ts
+++ src/PhaseRunner.ts
@@ -9,7 +9,7 @@ export class PhaseRunner<T> {
    * is one all children has a condition if there 2 or more
    * children.
    * 
-   * @param phase - Root phase
+   * @param phase Root phase
    */
   static valid<T> (phase: Phase<T>): boolean {
     const children = phase.children
@@ -39,8 +39,8 @@ export class PhaseRunner<T> {
    * Returns the indexes of phases that have been executed by
    * this PhaseRunner already
    * 
-   * @param phases - Phases to check index of
-   * @returns {Array<number>} - Array of indices
+   * @param phases Phases to check index of
+   * @returns {Array<number>} Array of indices
    */
   indexesOf (phases: Array<Phase<T>>): Array<number> {
     return phases.map(phase => this.indexOf(phase))
@@ -49,28 +49,29 @@ export class PhaseRunner<T> {
   /**
    * Validate the phase and all its children before executing
    * 
-   * @param phase - Root phase
-   * @param channel - Channel
-   * @param initialData - Data for the root phase
+   * @param phase Root phase
+   * @param channel Channel
+   * @param initialData Data for the root phase
+   * @param triggerMessage Message that triggered this phase
    */
-  async run (phase: Phase<T>, channel: ChannelInterface, initialData?: T, initialMessage?: MessageInterface): Promise<void> {
+  async run (phase: Phase<T>, channel: ChannelInterface, initialData?: T, triggerMessage?: MessageInterface): Promise<void> {
     if (!PhaseRunner.valid(phase)) {
       throw new Error('Invalid phase found. Phases with more than 1 child must have all its children to have a condition function specified.')
     }
-    return this.execute(phase, channel, initialData, initialMessage)
+    return this.execute(phase, channel, initialData, triggerMessage)
   }
 
   /**
    * Execute the phase without validating
    * 
-   * @param phase - Root phase
-   * @param channel - Channel
-   * @param initialData - Data for the root phase
+   * @param phase Root phase
+   * @param channel Channel
+   * @param initialData Data for the root phase
    */
-  async execute (initialPhase: Phase<T>, channel: ChannelInterface, initialData?: T, initialMessage?: MessageInterface): Promise<void> {
+  async execute (initialPhase: Phase<T>, channel: ChannelInterface, initialData?: T, triggerMessage?: MessageInterface): Promise<void> {
     this.ran.push(initialPhase)
     let thisPhase: Phase<T>|null = initialPhase
-    let thisMessage = initialMessage
+    let thisMessage = triggerMessage
     await thisPhase.sendUserFormatMessage(channel, initialData)
     while (thisPhase && thisPhase.shouldRunCollector()) {
       const {
@@ -89,9 +90,9 @@ export class PhaseRunner<T> {
     }
   }
 
-  static async run<T> (initialPhase: Phase<T>, channel: ChannelInterface, initialData?: T, initialMessage?: MessageInterface): Promise<PhaseRunner<T>> {
+  static async run<T> (initialPhase: Phase<T>, channel: ChannelInterface, initialData?: T, triggerMessage?: MessageInterface): Promise<PhaseRunner<T>> {
     const runner = new PhaseRunner<T>()
-    await runner.run(initialPhase, channel, initialData, initialMessage)
+    await runner.run(initialPhase, channel, initialData, triggerMessage)
     return runner
   }
 }
